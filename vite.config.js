0d4e3ebde4f4ae1a651147a36730a3c6bf2b3838
Mellydevs <119860983+Mellydevs@users.noreply.github.com>
2026-04-20 00:14:31 +0100

    add repo name to site

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    base: '/madebymel/',  // replace with your actual repo name
     outDir: '.',
     emptyOutDir: false  // important - stops it wiping your source files
   }
 });
+
